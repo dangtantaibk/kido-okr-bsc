492aff8a12f8f46bdb2b8556f3146fe726577b1f
TaiDang <tai.dt@kdc.vn>
2025-12-27 22:16:12 +0700

    feat: Add Dockerfile and .dockerignore for containerization setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  poweredByHeader: false,
 };
 
 export default nextConfig;
